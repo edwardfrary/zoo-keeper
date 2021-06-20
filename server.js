2526d3d6c96120e2a8c5d54053e58ac580c78a2d
edwardfrary <edwardfrary@gmail.com>
2021-06-20 07:10:59 -0400

    update

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 const { animals } = require('./data/animals.json');
 
 function validateAnimal(animal) {
@@ -77,7 +78,6 @@ function findById(id, animalsArray) {
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
-
     if (req.query) {
         results = filterByQuery(req.query, results);
     };
@@ -105,6 +105,22 @@ app.post('/api/animals', (req, res) => {
         res.json(animal);
     };
 
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) =>{
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) =>{
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
